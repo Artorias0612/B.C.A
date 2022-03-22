c1f2fb5bf37c322fa1cf47a96ba0a0219bb105ff
Artorias <96047479+Artorias0612@users.noreply.github.com>
2022-03-22 01:43:13 -0400

    Update Wallpaper.py

--- Wallpaper.py
+++ Wallpaper.py
@@ -1,12 +1,12 @@
 import os
 import time
 
-wallpaperlist = ['wallpaper.jpeg', 'wallpaper2.jpeg', 'wallpaper3.jpeg', 'wallpaper4.jpg', 'wallpaper5.jpg',
-                 'wallpaper6.jpeg']
-
-while True:
-
-    for chosen in wallpaperlist:
-
-        os.system(f'gsettings set org.gnome.desktop.background picture-uri file:////home/funlife/Pictures/Wallpapers/{chosen}')
-        time.sleep(60)
+try:
+    wallpaperlist = ['wallpaper.jpeg', 'wallpaper2.jpeg', 'wallpaper3.jpeg', 'wallpaper4.jpg', 'wallpaper5.jpg',
+                     'wallpaper6.jpeg']
+    while True:
+        for chosen in wallpaperlist:
+            os.system(f'gsettings set org.gnome.desktop.background picture-uri file:////home/funlife/Pictures/Wallpapers/{chosen}')
+            time.sleep(40)
+except KeyboardInterrupt:
+    exit()
